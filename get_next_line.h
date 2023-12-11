7ec8f23bf058224c2755a34f4c882657d18dcd9f
Pablo Markaide <pmarkaid@student.hive.fi>
2023-12-11 15:00:22 +0200

    feat: add project files

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 14:49:24 by pmarkaid          #+#    #+#             */
+/*   Updated: 2023/12/11 14:59:44 by pmarkaid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42 
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
