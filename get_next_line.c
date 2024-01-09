577bec4eb1fdf284cdffd4d7a1d2d969f83870e3
Pablo Markaide <pmarkaid@student.hive.fi>
2024-01-09 12:28:36 +0200

    feat: green for francinette, but using too long function

--- get_next_line.c
+++ get_next_line.c
@@ -6,68 +6,73 @@
 /*   By: pmarkaid <pmarkaid@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 14:56:05 by pmarkaid          #+#    #+#             */
-/*   Updated: 2023/12/15 15:35:48 by pmarkaid         ###   ########.fr       */
+/*   Updated: 2024/01/09 12:24:45 by pmarkaid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-char *load_buffer(int fd, char *remainder)
+char	*load_buffer(int fd, char *remainder)
 {
 	ssize_t	bytes_read;
 	size_t	remainder_len;
-	char buffer[BUFFER_SIZE];
-	char *new_buffer;
-	
+	char	buffer[BUFFER_SIZE];
+	char	*new_buffer;
+
 	remainder_len = 0;
 	new_buffer = NULL;
 	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	while(bytes_read > 0)
+	while (bytes_read > 0)
 	{
-		if(remainder)
+		if (remainder)
 			remainder_len = ft_strlenc(remainder, '\0');
 		new_buffer = malloc(sizeof(char) * (remainder_len + bytes_read + 1));
 		if (!new_buffer)
-			return(NULL);
+			return (NULL);
 		ft_memmove(new_buffer, remainder, remainder_len);
 		ft_memmove(new_buffer + remainder_len, buffer, bytes_read);
 		new_buffer[remainder_len + bytes_read] = '\0';
 		free(remainder);
 		remainder = new_buffer;
 		if (ft_strchr(new_buffer, '\n') != NULL || bytes_read < BUFFER_SIZE)
-			break;
+			break ;
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
+	}
+	if (bytes_read == -1)
+	{
+		free(remainder);
+		return (NULL);
 	}
 	return (remainder);
 }
 
-char *get_line2(char *remainder)
+char	*get_line2(char *remainder)
 {
-	size_t line_len;
-	char *line;
+	size_t	line_len;
+	char	*line;
 
 	if (!remainder)
 		return (NULL);
 	line_len = ft_strlenc(remainder, '\n') + 1;
 	line = malloc(sizeof(char) * (line_len + 1));
 	if (!line)
-		return(NULL);
+		return (NULL);
 	ft_memmove(line, remainder, line_len);
 	line[line_len] = '\0';
-	return(line);	
+	return (line);
 }
 
-char *update_remainder(char *remainder)
+char	*update_remainder(char *remainder)
 {
-	char *new_remainder;
-	char *new_line;
-	size_t remainder_len;
-	
+	char	*new_remainder;
+	char	*new_line;
+	size_t	remainder_len;
+
 	new_line = ft_strchr(remainder, '\n');
 	if (!new_line)
 	{
@@ -84,13 +89,13 @@ char *update_remainder(char *remainder)
 	}
 	ft_memmove(new_remainder, new_line + 1, remainder_len);
 	free(remainder);
-	return(new_remainder);	
+	return (new_remainder);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char *remainder;
-	char *line;
+	static char	*remainder;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -98,11 +103,16 @@ char *get_next_line(int fd)
 	if (!remainder || *remainder == '\0')
 	{
 		free(remainder);
+		remainder = NULL;
 		return (NULL);
 	}
 	line = get_line2(remainder);
 	if (!line)
-		return(NULL);
+	{
+		free(remainder);
+		remainder = NULL;
+		return (NULL);
+	}
 	remainder = update_remainder(remainder);
-	return(line);
+	return (line);
 }
